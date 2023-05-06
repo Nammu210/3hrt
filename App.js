6defd83dc0b8e74b9ec163d26160a4d9d9b8a815
Nammu210 <122613165+Nammu210@users.noreply.github.com>
2023-05-06 23:11:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Text from './Text';
+import Input from './Input';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Text/>
+    <Input/>
+   
+    </div>
+  );
+}
+export default App
